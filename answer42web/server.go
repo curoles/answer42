33698b684a1a5e8b1c0481314d1c8c350d8663b1
curoles <curoles@yahoo.com>
2018-01-15 23:58:49 -0800

    web, url->handler resolver with path and regexp packages

--- answer42web/server.go
+++ answer42web/server.go
@@ -5,17 +5,38 @@ import (
     "net/http"
     "os"
     "os/signal"
+    "path"
+    "regexp"
     "github.com/braintree/manners"
 )
 
-// Type associated with the handler.
+// Type associated with HTTP request handler function.
 // Implements http.Handler interface.
+// Also has map{key,handler} to find handler by URL path.
 type httpHandler struct {
+    handlers map[string]http.HandlerFunc
+    rexpHandlers map[string]http.HandlerFunc
+    rexpCache map[string]*regexp.Regexp
 }
 
 // Creates new instance of httpHandler and returns pointer to it.
 func newHttpHandler() *httpHandler {
-    return &httpHandler{}
+    return &httpHandler{
+        handlers     : make(map[string]http.HandlerFunc),
+        rexpHandlers : make(map[string]http.HandlerFunc),
+        rexpCache    : make(map[string]*regexp.Regexp),
+    }
+}
+
+// Add new pair {path, handler} to internal handler lookup map.
+func (h *httpHandler) Add(path string, handler http.HandlerFunc, isRegexp bool) {
+    if isRegexp == true {
+        h.rexpHandlers[path] = handler
+        compiledRexp, _ := regexp.Compile(path)
+        h.rexpCache[path] = compiledRexp
+    } else {
+        h.handlers[path] = handler
+    }
 }
 
 // Handles HTTP request, sends back a response.
@@ -23,13 +44,28 @@ func (h *httpHandler) ServeHTTP(
     rsp http.ResponseWriter,
     req *http.Request) {
 
-    query := req.URL.Query()
-    name := query.Get("name")
-    if name == "" {
-        name = "dear guest"
+    // Construct string "GET/POST URL"
+    methodAndPath := req.Method + " " + req.URL.Path
+
+    // Regexp match
+    for pattern, handlerFunc := range h.rexpHandlers {
+        if h.rexpCache[pattern].MatchString(methodAndPath) == true {
+            handlerFunc(rsp, req)
+            return
+        }
     }
 
-    fmt.Fprintf(rsp, "Answer is 42, %s", name)
+    // Simple "path" match
+    for pattern, handlerFunc := range h.handlers {
+        if mismatch, err := path.Match(pattern, methodAndPath); mismatch && err == nil {
+            handlerFunc(rsp, req)
+            return
+        } else if err != nil {
+            fmt.Fprint(rsp, err)
+        }
+    }
+
+    http.NotFound(rsp, req)
 }
 
 // Waits for OS shutdown signal and stops HTTP server.
@@ -47,6 +83,7 @@ func listenForShutdown(ch <-chan os.Signal) {
 //
 func runServer(prgOptions *ProgramOptions) {
     httpHandler := newHttpHandler()
+    httpHandler.Add("GET /*", mainHttpHandler, /*regexp:*/false)
 
     // Set up monitoring OS signals Interrupt and Kill.
     sigch := make(chan os.Signal)
@@ -57,4 +94,21 @@ func runServer(prgOptions *ProgramOptions) {
     hostAndPort := fmt.Sprintf("%s:%d", hostName, prgOptions.HttpPort)
     Logger.Println("Start serving on:", hostAndPort)
     manners.ListenAndServe(hostAndPort, httpHandler)
+}
+
+
+// ?.
+func mainHttpHandler(
+    rsp http.ResponseWriter,
+    req *http.Request) {
+
+    Logger.Println("HTTP request:", req.Method + " " + req.URL.RequestURI())
+
+    query := req.URL.Query()
+    name := query.Get("name")
+    if name == "" {
+        name = "dear guest"
+    }
+
+    fmt.Fprintf(rsp, "Answer is 42, %s", name)
 }
