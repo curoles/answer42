3b00c54753be959a5cc8877df042bc2ad4fdd3a6
curoles <curoles@yahoo.com>
2018-01-26 22:54:50 -0800

    playing with sqlite

--- answer42db/main.go
+++ answer42db/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
     "errors"
+    "strings"
     "strconv"
     "fmt"
+    "sync"
+    "os"
+    "io/ioutil"
+    "path/filepath"
+    "log"
     "database/sql"                    // https://golang.org/pkg/database/sql
     _ "github.com/mattn/go-sqlite3"
 )
@@ -36,8 +42,8 @@ func createDB(dbName string, dataSourceName string) (error) {
 
    createIdeaTable := "CREATE TABLE idea (" +
                       "id INTEGER PRIMARY KEY AUTOINCREMENT"+
-                      ", name varchar("+strconv.Itoa(ideaNameStrMaxLen)+") NOT NULL UNIQUE"+
-                      ", data varchar(32)"+
+                      ", name VARCHAR("+strconv.Itoa(ideaNameStrMaxLen)+") NOT NULL UNIQUE"+
+                      ", json JSON HIDDEN"+
                       ")"
 
    _,err = db.Exec(createIdeaTable)
@@ -52,27 +58,91 @@ func openDB(dbFilePath string) (*sql.DB, error) {
    return sql.Open(dbDriverName, dbFilePath)
 }
 
-func insertIdea(db *sql.DB, ideaName string) (int64, error) {
+func insertIdea(db *sql.DB, ideaName string, jsonText string) (int64, error) {
     if len(ideaName) > ideaNameStrMaxLen {
         return -1, errors.New(fmt.Sprintf("idea name is too long, %d > %d",
                               len(ideaName), ideaNameStrMaxLen))
     }
 
-    stmt, err := db.Prepare("INSERT INTO idea(name, data) values(?,?)")
+    stmt, err := db.Prepare("INSERT INTO idea(name, json) values(?,?)")
     if err != nil {
         return -1, err
     }
 
-    res, err := stmt.Exec(/*name:*/ideaName, /*data:*/"test string")
+    res, err := stmt.Exec(/*name:*/ideaName, /*json:*/jsonText)
     if err != nil {
         return -1, err
     }
+    defer stmt.Close()
 
     return res.LastInsertId()
 }
 
+func readIdeas(db *sql.DB, searchDirPath string) (error) {
+
+    fileList := []string{}
+    err := filepath.Walk(searchDirPath, func(path string, f os.FileInfo, err error) error {
+        if f.IsDir() != true && filepath.Ext(path) == ".json" {
+            fileList = append(fileList, path)
+        }
+        return err
+    })
+
+    if err != nil {
+        return err
+    }
+
+    var wg sync.WaitGroup
+    var mutex = &sync.Mutex{}
+
+    for _, fileName := range fileList {
+        wg.Add(1)
+        go func(fileName string) {
+            defer wg.Done()
+            ideaName := strings.TrimSuffix(filepath.Base(fileName), filepath.Ext(fileName))
+            log.Println("Inserting", ideaName)
+            jsonText, err := ioutil.ReadFile(fileName)
+            if err != nil {
+                return
+            }
+            mutex.Lock()
+            _, err = insertIdea(db, ideaName, string(jsonText))
+            mutex.Unlock()
+            if err != nil {
+                log.Println("can't insert into idea table:", err)
+            }
+            log.Println("Inserted", ideaName)
+            //wg.Done()
+        }(fileName)
+    }
+    log.Println("Waiting for all idea files to be processed...")
+    wg.Wait()
+    log.Println("Done")
+
+    return nil
+}
+
+func showIdeaTable(db *sql.DB) {
+	rows, err := db.Query("SELECT id, name, json FROM idea")
+	if err != nil {
+		panic(err)//log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+                var json string
+		err = rows.Scan(&id, &name, &json)
+		if err != nil {
+			panic(err)//log.Fatal(err)
+		}
+		fmt.Println(id, name, json)
+	}
+}
+
 func main() {
     dbFilePath := "./"+dbFileName
+    os.Remove(dbFilePath)
     if err := createDB(dbName, dbFilePath); err != nil {
         panic(err)
     }
@@ -83,10 +153,6 @@ func main() {
     }
     defer db.Close()
 
-    insertIdea(db, "idea1")
-    insertIdea(db, "idea2")
-    _, err = insertIdea(db, "idea1")
-    if err != nil {
-            fmt.Println("can't insert into idea table:", err)
-    }
+    readIdeas(db, "./src/github.com/curoles/answer42/dbsrc/idea")
+    //showIdeaTable(db)
 }
